29446867720e19cc23a089b10967d285ff1da380
wibastidas <wibastidas77@gmail.com>
2024-11-28 19:25:30 -0300

    fix: Correct business_id for test22 user in users.sql

--- flujoai-backend/src/scripts/sql/users.sql
+++ flujoai-backend/src/scripts/sql/users.sql
@@ -1,3 +1,3 @@
 INSERT INTO public.users (username,email,"password",business_id) VALUES
 	 ('admin','wibastidas@gmail.com','$2b$10$oee2ZMXsx6wRt4yOBiuKhuVmGdKusEFpRSky1BeEPNXjye7gjrgEO',1),
-	 ('test22','test22@gmail.com','$2b$10$qTd0ap.dbGwdVw5UIriuQu8YB3sPicY1gHFy.AiRczP1zwDhqT0Ru',16);
+	 ('test22','test22@gmail.com','$2b$10$qTd0ap.dbGwdVw5UIriuQu8YB3sPicY1gHFy.AiRczP1zwDhqT0Ru',7);
